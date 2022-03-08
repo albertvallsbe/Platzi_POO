fa931b9beaa92d24e9528c68ea02f3875d6e97b9
Albert <avalls89@gmail.com>
2022-03-08 17:47:30 +0100

    08/03/22

---
+++ Java/UberVan.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+class UberVan extends Car {
+    Map<String, Map<String, Integer>> typeCarAccepted;
+    ArrayList<String> seatsMaterial;
+    public Integer passenger;
+
+    
+    
+    public UberVan(String license, Account driver) {
+        super(license, driver);
+    }
+    
+    public UberVan(String license, Account driver, Map<String, Map<String, Integer>> typeCarAccepted,
+            ArrayList<String> seatsMaterial) {
+        super(license, driver);
+        this.typeCarAccepted = typeCarAccepted;
+        this.seatsMaterial = seatsMaterial;
+    }
+
+  
+
+    @Override
+    public void setPassenger(Integer passenger) {
+        if (passenger == 6) {
+            this.passenger = passenger;
+        } else {
+            System.out.println("Necesitas assignar 6 pasajeros");
+        }
+    }
+}
---
+++ Java/UberX.java
@@ -0,0 +1,19 @@
+class UberX extends Car {
+    String brand;
+    String model;
+
+    public UberX(String license, Account driver, String brand, String model) {
+        super(license, driver);
+        this.brand = brand;
+        this.model = model;
+
+    }
+
+    @Override
+    void prindDataCar() {
+        
+        super.prindDataCar();
+        System.out.println("Modelo: "+ model + " Brand: " + brand);
+    }
+
+}
